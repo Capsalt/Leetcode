507ae2be48e59e7e38bc9def6cd0f695d36443e2
Hasan Basri Aygümüş <hbasri2304@gmail.com>
2022-03-26 19:55:43 +0300

    Update 704.BinarySearch.cs

--- C# Solution/704.BinarySearch.cs
+++ C# Solution/704.BinarySearch.cs
@@ -11,3 +11,33 @@ public class Solution {
         return -1;
     }
 }
+
+
+//Second Solution
+
+public class Solution {
+    public int Search(int[] nums, int target) {
+         
+            int end = nums.Length - 1;
+            int begin = 0;
+            int ort = (end + begin) / 2;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[ort] == target) return ort;
+
+                if (nums[ort]>target)
+                {
+                    end = ort - 1;
+                    
+                }
+                if (nums[ort]<target)
+                {
+                    begin = ort + 1;
+                    
+                }
+                ort = (end + begin) / 2;
+            }
+            return -1;
+    }
+}
